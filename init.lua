57d5d2ffae4f28d74df1b4fd83503bbc82be20b4
dvquentincarrel <dvquentincarrel@gmail.com>
2024-01-22 16:31:09 +0100

    [FA] Fix augroup for TS start, add hl on yank

--- init.lua
+++ init.lua
@@ -5,7 +5,12 @@ vim.cmd('source ~/.vim/vimrc')
 
 local TSgroup = vim.api.nvim_create_augroup('vimrc', {clear = true})
 vim.api.nvim_create_autocmd({'Filetype'}, {
-    pattern = '*.py,*.sh,*.vim,*.json',
+    pattern = 'python,sh,bash,vim,json,html',
     group = TSgroup,
     command = 'lua vim.treesitter.start()'
+})
+
+local HLYank = vim.api.nvim_create_autocmd({'TextYankPost'}, {
+    pattern = '*',
+    command = 'lua vim.highlight.on_yank({on_visual=false, timeout=750})'
 })
