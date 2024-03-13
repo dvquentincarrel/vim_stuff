4836ee40ea05356ba6fe1e6672ad76906f0c4db4
dvquentincarrel <dvquentincarrel@gmail.com>
2024-03-13 18:53:29 +0100

    [A] Tree-sitter injection for manual onchanges

---
+++ treesitter/html/injections.scm
@@ -0,0 +1,11 @@
+; <field name="raw_code|code"> ... </field>
+(element
+  (start_tag
+   ((tag_name) @_tag (#eq? @_tag "field"))
+   (attribute 
+      ((attribute_name) @_attr @text.title (#eq? @_attr "name"))
+      (quoted_attribute_value (attribute_value) @_a_val (#any-of? @_a_val "code" "raw_code"))))
+  ((text) @injection.content
+   (#set! injection.language "python"))
+)
+; vim: ft=query
