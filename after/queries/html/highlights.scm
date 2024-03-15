96a5f647c698f2f4a5d5fbf5bce8dda7b185e63b
dvquentincarrel <dvquentincarrel@gmail.com>
2024-03-15 13:29:46 +0100

    [A] treesitter xml name/onchange highlights

---
+++ after/queries/html/highlights.scm
@@ -0,0 +1,29 @@
+; inherits html
+
+; <button name="xxxx" .../> | <label name="xxxx" .../>
+(
+((tag_name) @_tag
+    (#any-of? @_tag "button" "label"))
+(attribute  
+    ((attribute_name) @_attr
+        (#eq? @_attr "name"))
+    (quoted_attribute_value (attribute_value) @markup.strong @constant)))
+
+; <field name="xxxx" ... type="xxx"/> | <field name="xxxx" ... type="xxx"> ... </field>
+(
+((tag_name) @_tag
+    (#eq? @_tag "field"))
+(attribute  
+    ((attribute_name) @_attr
+        (#eq? @_attr "name"))
+    (quoted_attribute_value (attribute_value) @markup.strong @constant))
+(attribute
+    ((attribute_name) @_attr2
+        (#eq? @_attr2 "type")))
+)
+
+; <xxx onchange="xxxx"/>
+(attribute  
+    ((attribute_name) @_attr
+        (#eq? @_attr "onchange"))
+    (quoted_attribute_value (attribute_value) @markup.strong @keyword.operator))
