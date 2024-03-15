fbe14b8fa79d3d7735416ed49f570746b8226147
dvquentincarrel <dvquentincarrel@gmail.com>
2024-03-15 14:24:21 +0100

    [A] treesitter injection for sql in python

---
+++ after/queries/python/injections.scm
@@ -0,0 +1,28 @@
+; inherits python
+
+; cr.execute(""" ... """)
+(call
+    function: ((attribute) @f_name (#match? @f_name "cr.execute"))
+    arguments: (argument_list
+        (string 
+            (string_start)
+            ((string_content) @injection.content
+                (#set! injection.language "sql"))
+        )
+    )
+)
+
+; cr.execute(""" ... """ % (...))
+(call
+    function: ((attribute) @f_name (#match? @f_name "cr.execute"))
+    arguments: (argument_list
+        (binary_operator
+            (string 
+                (string_start)
+                ((string_content) @injection.content
+                    (#set! injection.language "sql"))
+            )
+        )
+    )
+)
+; vim: ft=query
