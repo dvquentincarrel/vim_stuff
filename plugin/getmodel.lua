ce831d3b563a4712d9866547c5e1680b39937eb5
dvquentincarrel <dvquentincarrel@gmail.com>
2024-04-26 12:53:14 +0200

    [F] getmodel: Use editor size instead of window's size

--- plugin/getmodel.lua
+++ plugin/getmodel.lua
@@ -1,10 +1,10 @@
 get_model = function(width_percent, height_percent)
     -- Sets up window
     local newbuf = vim.api.nvim_create_buf(false, true)
-    local width = math.floor(vim.api.nvim_win_get_width(0) * (width_percent / 100) + 0.5)
-    local height = math.floor(vim.api.nvim_win_get_height(0) * (height_percent / 100) + 0.5)
-    local col = math.floor((vim.api.nvim_win_get_width(0) - width) / 2)
-    local row = math.floor((vim.api.nvim_win_get_height(0) - height) / 2)
+    local width = math.floor(vim.o.columns * (width_percent / 100) + 0.5)
+    local height = math.floor(vim.o.lines * (height_percent / 100) + 0.5)
+    local col = math.floor((vim.o.columns - width) / 2)
+    local row = math.floor((vim.o.lines - height) / 2)
     local new_win = vim.api.nvim_open_win(newbuf, true, {relative='editor', row=row, col=col, width=width, height=height, border='rounded', style='minimal'})
 
     -- Callback when the script is done running
