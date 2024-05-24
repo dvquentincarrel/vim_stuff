b4206344534a141967c0afaaf33cefb5ad633d9a
dvquentincarrel <dvquentincarrel@gmail.com>
2024-05-24 17:45:03 +0200

    [M] dap: change layout, add more keybinds

--- lua/init_dap.lua
+++ lua/init_dap.lua
@@ -1,16 +1,59 @@
 dap = require("dap")
 dapui = require("dapui")
 dapui.setup()
+dapui.setup({
+    layouts= {
+        {
+            elements = {{
+                id = "breakpoints",
+                size = 0.10
+            }, {
+                id = "stacks",
+                size = 0.90
+            }},
+            position = "left",
+            size = 40
+        },
+        {
+            elements = {{
+                id = "watches",
+                size = 0.50
+            }, {
+                id = "scopes",
+                size = 0.50
+            }},
+            position = "right",
+            size = 40
+        },
+        {
+            elements = {{
+                id = "repl",
+                size = 1.0
+            }},
+            position = "bottom",
+            size = 10
+        }
+    }
+})
 
-vim.keymap.set('n', '<F8>', function() dap.continue() end)
+vim.keymap.set('n', '<F8>', function()
+    vim.o.switchbuf = 'uselast'
+    dap.continue() 
+end)
 vim.keymap.set('n', '<F10>', function() dap.step_over() end)
 vim.keymap.set('n', '<F11>', function() dap.step_into() end)
 vim.keymap.set('n', '<F12>', function() dap.step_out() end)
-vim.keymap.set('n', '<Leader>dB', function() dap.toggle_breakpoint() end)
-vim.keymap.set('n', '<Leader>dbl', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
-vim.keymap.set('n', '<Leader>dL', function() dap.run_last() end)
-vim.keymap.set('n', '<Leader>dut', function() dapui.toggle() end)
+vim.keymap.set('n', '<Leader>db', function() dap.toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>dr', function() dap.run_to_cursor() end)
+vim.keymap.set('n', '<Leader>dp', function() dap.goto_() end)
+vim.keymap.set('n', '<Leader>df', function() dap.focus_frame() end)
+vim.keymap.set('n', '<Leader>dt', function() dapui.toggle() end)
+vim.keymap.set('n', '<Leader>dT', function() dapui.toggle({layout=1}) end)
 vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
-  require('dap.ui.widgets').hover()
+    require('dap.ui.widgets').hover()
 end)
+
+ok, dappy = pcall(require, 'dap-python')
+if ok then
+    dappy.setup()
+end
