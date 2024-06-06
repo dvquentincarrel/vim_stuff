ca8307db7aec54d73cc194cd1ab9e4fb4311ef6e
dvquentincarrel <dvquentincarrel@gmail.com>
2024-06-06 12:48:08 +0200

    [M] Set fzflua preview to vertical

--- lua/init_fzflua.lua
+++ lua/init_fzflua.lua
@@ -3,4 +3,6 @@ if ok then
     fzflua.setup {}
     fzflua.config.defaults.keymap.builtin["<C-p>"] = "toggle-preview"
     fzflua.config.defaults.keymap.builtin["<M-f>"] = "toggle-fullscreen"
+    fzflua.config.defaults.winopts.preview.layout = "vertical"
+    fzflua.config.defaults.winopts.preview.vertical = "down:80%"
 end
