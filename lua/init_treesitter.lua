873a852279ae63b5df3b95fdf3b9ea5b97ffac33
dvquentincarrel <dvquentincarrel@gmail.com>
2024-08-01 04:43:08 +0200

    [A] Treesitter refactoring & text objects

--- lua/init_treesitter.lua
+++ lua/init_treesitter.lua
@@ -1,13 +1,42 @@
-require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "python", "bash", "vim", "json", "html",
-                         "markdown", "markdown_inline", "make", "sql" },
-    highlight = {
-        enable = true ,
-        disable = function(lang, buf)
-            return vim.api.nvim_buf_line_count(buf) >= 15000
-        end
+function disable_cond(lang, buf) -- Disable treesitter for files larger than 15k lines
+    return vim.api.nvim_buf_line_count(buf) >= 15000
+end
 
+require 'nvim-treesitter.configs'.setup {
+    incremental_selection = {
+        enable = true,
+        disable = disable_cond,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            node_decremental = "grm",
+            scope_incremental = false,
+        },
     },
-    incremental_selection = { enable = true },
-    textobjects = { enable = true },
+    indent = { enable = true, disable = disable_cond },
+    highlight = { enable = true, disable = disable_cond },
+    refactor = {
+        highlight_definitions = {
+            enable = true,
+            disable = disable_cond,
+            clear_on_cursor_move = true,
+        },
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+                ["af"] = { query = "@function.outer", desc = "Select outer function"},
+                ["if"] = { query = "@function.inner", desc = "Select inner function"},
+                ["ac"] = { query = "@class.outer", desc = "Select outer class"},
+                ["ic"] = { query = "@class.inner", desc = "Select inner class" },
+            },
+            selection_modes = {
+                ['@function.outer'] = 'V', -- linewise
+                ['@class.outer'] = 'V', -- blockwise
+            },
+        },
+    },
 }
