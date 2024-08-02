01d2bb7e8011781b638263d439d3834631907c94
dvquentincarrel <dvquentincarrel@gmail.com>
2024-08-02 18:10:01 +0200

    [A] Add descriptions to keymaps

--- lua/init_dap.lua
+++ lua/init_dap.lua
@@ -7,10 +7,11 @@ dapui.setup({
             elements = {{
                 id = "breakpoints",
                 size = 0.10
-            }, {
-                id = "stacks",
-                size = 0.90
-            }},
+            },
+                {
+                    id = "stacks",
+                    size = 0.90
+                }},
             position = "left",
             size = 40
         },
@@ -19,9 +20,9 @@ dapui.setup({
                 id = "watches",
                 size = 0.50
             }, {
-                id = "scopes",
-                size = 0.50
-            }},
+                    id = "scopes",
+                    size = 0.50
+                }},
             position = "right",
             size = 40
         },
@@ -40,18 +41,58 @@ vim.keymap.set('n', '<F8>', function()
     vim.o.switchbuf = 'uselast'
     dap.continue() 
 end)
-vim.keymap.set('n', '<F10>', function() dap.step_over() end)
-vim.keymap.set('n', '<F11>', function() dap.step_into() end)
-vim.keymap.set('n', '<F12>', function() dap.step_out() end)
-vim.keymap.set('n', '<Leader>db', function() dap.toggle_breakpoint() end)
-vim.keymap.set('n', '<Leader>dr', function() dap.run_to_cursor() end)
-vim.keymap.set('n', '<Leader>dp', function() dap.goto_() end)
-vim.keymap.set('n', '<Leader>df', function() dap.focus_frame() end)
-vim.keymap.set('n', '<Leader>dt', function() dapui.toggle() end)
-vim.keymap.set('n', '<Leader>dT', function() dapui.toggle({layout=1}) end)
-vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
-    require('dap.ui.widgets').hover()
-end)
+vim.keymap.set(
+    'n',
+    '<F10>',
+    function() dap.step_over() end,
+    {desc = "DAP step over"}
+)
+vim.keymap.set('n',
+    '<F11>',
+    function() dap.step_into() end,
+    {desc = "DAP step into"}
+)
+vim.keymap.set('n',
+    '<F12>',
+    function() dap.step_out() end,
+    {desc = "DAP step out"}
+)
+vim.keymap.set('n',
+    '<Leader>db',
+    function() dap.toggle_breakpoint() end,
+    {desc = "DAP step toggle breakpoints"}
+)
+vim.keymap.set('n',
+    '<Leader>dr',
+    function() dap.run_to_cursor() end,
+    {desc = "DAP run to cursor"}
+)
+vim.keymap.set('n',
+    '<Leader>dp',
+    function() dap.goto_() end,
+    {desc = "DAP goto cursor"}
+)
+vim.keymap.set('n',
+    '<Leader>df',
+    function() dap.focus_frame() end,
+    {desc = "DAP focus frame (cursor position)"}
+)
+vim.keymap.set('n',
+    '<Leader>dt',
+    function() dapui.toggle() end,
+    {desc = "DAP Toggle UI"}
+)
+vim.keymap.set('n',
+    '<Leader>dT',
+    function() dapui.toggle({layout=1}) end,
+    {desc = "DAP Toggle UI left panes"}
+)
+vim.keymap.set({'n',
+    'v'},
+    '<Leader>dh',
+    function() require('dap.ui.widgets').hover() end,
+    {desc = "DAP evaluate hovered/selected expression"}
+)
 
 local ok
 ok, dappy = pcall(require, 'dap-python')
--- lua/init_luasnip.lua
+++ lua/init_luasnip.lua
@@ -1,19 +1,32 @@
 local ls = require("luasnip")
 
-vim.keymap.set({"i", "s"}, "<C-K>", function()
-    if ls.expand_or_jumpable() then
-        ls.expand_or_jump()
-    end
-end, {silent = true})
+vim.keymap.set(
+    {"i", "s"}, "<C-K>",
+    function()
+        if ls.expand_or_jumpable() then
+            ls.expand_or_jump()
+        end end,
+    {silent = true, desc = "Expand a snippet, or goto next var of snippet"})
 
-vim.keymap.set({"i", "s"}, "<C-J>", function()
-    ls.jump(-1)
-end, {silent = true})
+vim.keymap.set(
+    {"i", "s"},
+    "<C-J>",
+    function() ls.jump(-1) end,
+    {silent = true, desc = "Goto prev var of snippet"})
 
-vim.keymap.set({"i", "s"}, "<C-C>", function()
-	if ls.choice_active() then
+vim.keymap.set(
+    {"i", "s"},
+    "<C-C>",
+    function()
+    if ls.choice_active() then
         require("luasnip.extras.select_choice")()
-	end
-end, {silent = true})
+    end end,
+    {silent = true, desc = "Cycle through options for snippet var"})
 
-require("luasnip.loaders.from_snipmate").load({paths=vim.env.GIT_PATH..'/snippets'})
+function reload_snippets(t)
+    paths = t[1] or vim.env.GIT_PATH..'/snippets'
+    require("luasnip.loaders.from_snipmate").load({paths=paths})
+end
+
+reload_snippets{}
+vim.api.nvim_create_user_command('LuaSnipReloadSnippets', function(t) reload_snippets{t.args} end, {})
