3c9f81528ad101532140534c0118638d8a2fff9b
dvquentincarrel <dvquentincarrel@gmail.com>
2024-08-09 17:31:50 +0200

    [M] Ad keybind for general fzflua menu

--- lua/init_fzflua.lua
+++ lua/init_fzflua.lua
@@ -9,4 +9,10 @@ if ok then
     fzflua.config.defaults.keymap.builtin["<M-f>"] = "toggle-fullscreen"
     fzflua.config.defaults.winopts.preview.layout = "vertical"
     fzflua.config.defaults.winopts.preview.vertical = "down:80%"
+
+    vim.keymap.set(
+        {"n"},
+        "<leader>fz",
+        ":FzfLua<cr>",
+        {silent = true, desc = "Opens FzfLua"})
 end
