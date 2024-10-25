4483785d079b6ee094248ad0e1c106367c1d691d
Quentin Carrel <dvquentincarrel@gmail.com>
2024-10-25 15:57:59 +0200

    [A] lsp: add descriptions to keybinds, change goto declaration to goto typedef

--- lua/init_lsp.lua
+++ lua/init_lsp.lua
@@ -18,7 +18,7 @@ local lsp_zero = require('lsp-zero')
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver',},
+  ensure_installed = {},
   handlers = {
     lsp_zero.default_setup,
   }
@@ -34,26 +34,25 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
 
-    vim.keymap.set("n", "K", vim.lsp.buf.hover) -- Same as nvim-lspconfig
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts) -- Same as nvim-lspconfig
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- Same as nvim-lspconfig
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- Same as nvim-lspconfig
-    vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts) -- Same as nvim-lspconfig
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts) -- Same as nvim-lspconfig
-    vim.keymap.set("n", "grn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Get information about the hovered symbol" }) -- Same as nvim-lspconfig
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = ev.buf, desc = "Get signature help"}) -- Same as nvim-lspconfig
+    vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, { buffer = ev.buf, desc = "Goto type defintion"})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "Goto definition"}) -- Same as nvim-lspconfig
+    vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { buffer = ev.buf, desc = "Goto implementation"}) -- Same as nvim-lspconfig
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = ev.buf, desc = "Find references"}) -- Same as nvim-lspconfig
+    vim.keymap.set("n", "grn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename symbol"})
 
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, { buffer = ev.buf, desc = "Add workspace folder"})
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, { buffer = ev.buf, desc = "Remove workspace folder"})
     vim.keymap.set('n', '<space>wl', function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
+    end, { buffer = ev.buf, desc = "List workspace folders"})
 
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts) -- Same as nvim-lspconfig
-    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts) -- Same as nvim-lspconfig
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = ev.buf, desc = "Find references"}) -- Same as nvim-lspconfig
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = ev.buf, desc = "Use code-action"}) -- Same as nvim-lspconfig
     vim.keymap.set('n', '<space>glf', function()
       vim.lsp.buf.format { async = true }
-    end, opts)
+    end, { buffer = ev.buf, desc = "Format file"})
   end,
 })
