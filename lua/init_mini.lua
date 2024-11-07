228b22e66ec5928ef70f43b729bff0cbb50f3d32
Quentin Carrel <dvquentincarrel@gmail.com>
2024-11-07 16:23:27 +0100

    [A] mini.surround: add dictionary item

--- lua/init_mini.lua
+++ lua/init_mini.lua
@@ -1,2 +1,14 @@
 vim.cmd.packadd('mini.surround')
-require('mini.surround').setup()
+require('mini.surround').setup({
+    custom_surroundings = {
+        ['d'] = {
+            input = {
+                [=[%f[%w_][%w_]+%[['"]()[%S]+()['"]%]]=]
+            },
+            output = function()
+                local dict_name = MiniSurround.user_input('Dictionary name')
+                return { left = dict_name .. "['", right = "']"}
+            end,
+        }
+    }
+})
