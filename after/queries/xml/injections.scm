0953cdfc133a31ac26c3e4fb1cb717b08d89c009
Quentin Carrel <dvquentincarrel@gmail.com>
2024-11-26 10:35:09 +0100

    [F] query inject: fix structure, add troubleshooting info

--- after/queries/xml/injections.scm
+++ after/queries/xml/injections.scm
@@ -11,6 +11,8 @@
                 (#set! injection.language "python"))))
 
 ; <field name="raw_code|code"><![CDATA[ ... ]]></field>
+; BEWARE! The CDATA highlight in the default query has a priority. It must be
+; removed for this to work (seemingly can't put priorities on injections)
 (element
   (STag  
     ((Name) @_e_name (#eq? @_e_name "field"))
@@ -19,7 +21,7 @@
       ((AttValue) @_a_val (#any-of? @_a_val "\"code\"" "\"raw_code\""))))
   (content 
     (CDSect 
+      (CDStart)
       ((CData) @injection.content
-               (#set! injection.language "python"))))
-  (#set! "priority" 110))
+               (#set! injection.language "python")))))
 ; vim: ft=query
