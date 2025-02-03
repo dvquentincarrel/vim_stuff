dd2859ee9ae9cfc1819736b6f56041ab3f9dc226
Quentin Carrel <dvquentincarrel@gmail.com>
2025-02-03 08:32:47 +0100

    [A] mini.surround: added 'a' for ANSI CSI wrapping

--- lua/init_mini.lua
+++ lua/init_mini.lua
@@ -10,13 +10,19 @@ require('mini.surround').setup({
                 local dict_name = MiniSurround.user_input('Dictionary name')
                 return { left = dict_name .. "['", right = "']"}
             end,
+        },
+        ['a'] = { -- Ansi csi wrapping
+            output = function()
+                local ansi_csi_code = MiniSurround.user_input('ANSI CSI code')
+                return { left = '\\x1b[' .. ansi_csi_code .. "m", right = "\\x1b[m"}
+            end,
         }
     }
 })
 require('mini.indentscope').setup{
     draw={
-        animation=require('mini.indentscope').gen_animation.quadratic{
-            easing='in-out',
+        animation=require('mini.indentscope').gen_animation.exponential{
+            easing='out',
             duration=40,
         }
     }
