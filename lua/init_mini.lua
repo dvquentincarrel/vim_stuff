1ba67fc8cbe0831d622586b6063ea9033725f248
Quentin Carrel <dvquentincarrel@gmail.com>
2025-02-28 21:38:21 +0100

    [A] mini surround: Lisp procedure obj, CSI input, comments

--- lua/init_mini.lua
+++ lua/init_mini.lua
@@ -2,19 +2,37 @@ vim.cmd.packadd('mini.surround')
 vim.cmd.packadd('mini.indentscope')
 require('mini.surround').setup({
     custom_surroundings = {
-        ['d'] = {
+        ['d'] = { -- Python dict, balanced square brackets
             input = {
-                [=[%f[%w_][%w_]+%[['"]()[%S]+()['"]%]]=]
+                '%f[%w][%w]+%b[]', [=[^.-%[['"]().*()['"].$]=]
             },
             output = function()
                 local dict_name = MiniSurround.user_input('Dictionary name')
-                return { left = dict_name .. "['", right = "']"}
+                if dict_name then
+                    return { left = dict_name .. "[", right = "]"}
+                end
+            end,
+        },
+        ['p'] = { -- Lisp procedure, balanced parens
+            input = {
+                '%b()' ,'^.[%S]+[%s]+().*().'
+            },
+            output = function()
+                local procedure_name = MiniSurround.user_input('Procedure name')
+                if procedure_name then
+                    return { left = "(" .. procedure_name .. " ", right = ")" }
+                end
             end,
         },
         ['a'] = { -- Ansi csi wrapping
+            input = {
+                '\\x1b.-m().*()\\x1b.-m'
+            },
             output = function()
                 local ansi_csi_code = MiniSurround.user_input('ANSI CSI code')
-                return { left = '\\x1b[' .. ansi_csi_code .. "m", right = "\\x1b[m"}
+                if ansi_csi_code then
+                    return { left = '\\x1b[' .. ansi_csi_code .. "m", right = "\\x1b[m"}
+                end
             end,
         }
     }
